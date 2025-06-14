e39c81ebac89b030bd23e25d17560a93a4091b06
MiraclePositivibes <miraclepositivibes@gmail.com>
2025-06-14 23:59:34 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: false,
     rollupOptions: {
       input: 'src/main.jsx',
     },
+    assetsDir: 'assets',
+    emptyOutDir: true,
   },
   server: {
     port: 3000,

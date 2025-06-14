da79c76d67ce6291f660560f8fd421cb65fd1c98
MiraclePositivibes <miraclepositivibes@gmail.com>
2025-06-14 23:29:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
+    outDir: 'dist',
+    sourcemap: false,
     rollupOptions: {
-      input: "src/main.jsx",
+      input: 'src/main.jsx',
     },
+  },
+  server: {
+    port: 3000,
+    host: true,
   },
 });
